3ac87df82069094561ad6c877b76d03ba4016e43
eris-he <foreverphysics7@gmail.com>
2023-05-23 09:49:05 -0500

    website commit 2

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,11 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("css/Site.css");
 	eleventyConfig.addPassthroughCopy("scripts/Site.js");
+    eleventyConfig.addPassthroughCopy("scripts/nav.js");
+    eleventyConfig.addPassthroughCopy("nav.html");
+    eleventyConfig.addPassthroughCopy("contact.html");
+    eleventyConfig.addPassthroughCopy("pens.html");
+    eleventyConfig.addPassthroughCopy("scripts/index.js");
+    eleventyConfig.addPassthroughCopy("css/index.css");
+    eleventyConfig.addPassthroughCopy("hobbies.html");
 };
